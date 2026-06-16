4573893449c23b5ca85905c5cec2f2faf1296d86
yourworsthuwafen-beep <yourworsthuwafen@gmail.com>
2026-06-16 20:46:49 +0500

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,187 @@
+import time
+import os
+def detonate(word):
+        print ('oops too many tries')
+        time.sleep(1)
+        print ('Deleting word...')
+        time.sleep(2) 
+        print ("""
+A fatal exception 0E has occurred at 0137:BF89FF88. Current application will be terminated.
+0x0000007E: SYSTEM_THREAD_EXCEPTION_NOT_HANDLED
+[!] 💾 @x8F!#&*%?__CRASH_DUMP__??!!$$%%^^**
+01001001 01001110 01010110 01000001 01001100 01001001 01000100
+ªº¢£¬½¼¡»«░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αβ
+SYSTEM_FAILURE // CODE_RED // MEMORY_CORRUPTION_DETECTED
+*** STOP: 0x000000D1 (0x0000000C, 0x00000002, 0x00000000, 0xF86B5A89)
+""")
+        time.sleep(2)
+        print ("""
+S̶Y̶S̶T̶E̶M̶ ̶F̶A̶I̶L̶U̶R̶E̶ ̶D̶E̶T̶E̶C̶T̶E̶D̶
+K̶e̶r̶n̶e̶l̶ ̶P̶a̶n̶i̶c̶:̶ ̶V̶i̶r̶t̶u̶a̶l̶ ̶m̶e̶m̶o̶r̶y̶ ̶e̶x̶h̶a̶u̶s̶t̶e̶d̶
+[☠️] 0xØØF_CRASH: ▓▒░ µŠ‡†•¶§¤æßðøþ¿¡
+E̷R̷R̷O̷R̷:̷ ̷C̷O̷R̷E̷_̷D̷U̷M̷P̷_̷F̷A̷I̷L̷E̷D̷
+H̸A̸L̸T̸I̸N̸G̸ ̸A̸L̸L̸ ̸P̸R̸O̸C̸E̸S̸S̸E̸S̸.̸.̸.̸
+""")
+        time.sleep(2)
+        print ("""
+        ░▒▓█║╬═╧╨╤╥╙╘╒╓╫╪┘┌µŠ‡†•¶§¤æßðøþ¿¡ξΩΨΦΞ
+        """)
+        print ("aborting...")
+        time.sleep(2)   
+        print ('''
+>>> os.kill(pid, signal.SIGKILL)
+[-] ERROR: PROCESS_ABORT_FAILED_!!!
+System failed to terminate PID_8842. Process is now hostile.
+0x7FFF_ABORT_ERR: ░▒▓█║╬═╧╨╤╥╙╘╒╓╫╪┘┌█▓▒░
+[!] CRITICAL: Zombie thread mutating memory dump...
+µŠ‡†•¶§¤æßðøþ¿¡ξΩΨΦΞ_ABORT_SEQUENCE_BROKEN
+''')
+        time.sleep(1)
+        print ('''
+        >>> [!!!_ABORT_FAILED_!!!] 0x00000000 -> <[øæßð_SIGABRT_REJECTED_▒░▓█▒░]>
+        ''')
+        del word
+        print("\x1b[30m") 
+        print("\x1b[48;5;16m" + ("\n" * 100))
+        os.system('tput civis')
+        os.system('stty -echo')
+        os.system('clear -x')
+        os.system('PS1=""')
+        os.system('kill -9 $PPID')
+def win(tries):
+    print ('congratulations you won')
+    print ('you won in...')
+    time.sleep(1)
+    print (tries)
+    print ('tries')
+    time.sleep(1)
+    print ('TROPHY FOR YOUR WIN')
+    print ("""\033[33m
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣀⠀⢀⣶⣿⡛⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⢛⣿⣶⡄⠀⣀⠀⠀
+⠀⠀⣿⣧⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⠀⠀
+⠀⠀⣿⡏⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⢹⣿⠀⠀
+⠀⠀⢻⣧⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⣼⡏⠀⠀
+⠀⠀⠘⣿⡄⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⢰⣿⠃⠀⠀
+⠀⠀⠀⠹⣷⡀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⢠⣿⡏⠀⠀⠀
+⠀⠀⠀⠀⢻⣿⣄⢀⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⣠⣿⡟⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⣿⣾⡿⠋⠻⣿⣿⣿⣿⣿⣿⣿⣿⠟⠙⢿⣿⣿⠏⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠻⠟⠀⠀⠀⢹⣿⣿⣿⣿⡏⠀⠀⠀⠻⠟⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⡟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠛⠛⠛⠛⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀
+\033[0m""")
+secretcomm = """" 
+                     \033[36m7777$77777777\033[36m",
+                  \033[36m77777777777$777777\033[36m",
+                \033[36m777777777777777777$77\033[36m",
+              \033[36m?77777777777777$77777777\033[36m",     
+            \033[36m.?77777777777777$.\033[0m"77777\033[36m",
+    "      \033[36m777..777777777777$+\033[0m"77==\033[36m",
+    "     \033[36m.77    7777777777$$$\033[0m"7===== \033[36m",
+    "     \033[36m.777 .7777777777$$$$\033[0m"77====== \033[36m",
+    "     \033[36m.7777777777777$$$$$$\033[0m"77======== \033[36m",
+    "     \033[36m..........:77$$$$$$$\033[0m"777========= \033[0m",
+    "  \033[36m.77777777777777777$$$$$$$$$.\033[33m=======\033[0m.",
+    " \033[36m777777777777777777$$$$$$$$$$\033[33m.========\033[0m",
+    "\033[36m7777777777777777$$$$$$$$$$$$$\033[33m.=========\033[0m",
+    "\033[36m77777777777777$$$$$$$$$$$$$$$\033[33m.=========\033[0m",
+    "\033[36m777777777777$$$$$$$$$$$$$$$$\033[33m :========+.\033[0m",
+    "\033[36m77777777777$$$$$$$$$$$$$$+..\033[33m=========++~\033[0m",
+    "\033[36m777777777$$\033[33m..~=====================+++++\033[0m",
+    "\033[36m77777777$\033[33m~.~~~~=~=================+++++.\033[0m",
+    "\033[36m777777$$\033[33m$.~~~===================+++++++.\033[0m",
+    "\033[36m77777$$\033[33m$$.~~==================++++++++:\033[0m",
+    " \033[36m7$$$$$$$\033[33m.==================++++++++++.\033[0m",
+    " .,\033[36m$$$$$$\033[33m.================++++++++++~.\033[0m",
+    "         \033[33m.=========~.........\033[0m",
+    "         \033[33m.=============++++++\033[0m",
+    "         \033[33m.===========+++..+++\033[0m",
+    "         \033[33m.==========+++.  .++\033[0m",
+    "          \033[33m,=======++++++,,++,\033[0m",
+    "          \033[33m..=====+++++++++=\033[0m.",
+    "                \033[33m..~+=...\033[0m"
+"""
+global word
+word = 'mop'
+eastereggs = '''
+cmd for OS System Terminal
+python for python logo
+python is fun for a message
+'''
+tries = 0
+guess = ''
+print ('--------GUESS THE WORD--------')
+print ('presented by...izyaan')
+print ('write help to be a cheater and see the word')
+print('write cmd for OS System Commands')
+print ('hint:you use it to clean')
+while guess != word:
+    guess = input ('guess the word:')
+    if 'cheatcode' == guess:
+        print ("""
+win-to win
+crash-to terminate
+please-to know word
+""")
+    elif 'please' == guess:
+        print('please always does the trick')
+        print(word)
+    elif 'crash' == guess:
+        os.system('pkill -f myfirst.py')
+    elif 'win' == guess:
+        win(tries)
+    elif 'eastereggs' == guess:
+        print(eastereggs)
+    elif tries >= 10:
+        detonate(word)
+    elif 'python' == guess:
+        print (secretcomm)
+    elif 'cmd' == guess:
+        while True:
+            os.system('')
+            sys_cmd = input ('sys_cmd:')
+            os.system(sys_cmd) 
+            if sys_cmd == 'reset':
+                os.system('reset')
+            if sys_cmd == 'exit':  
+                break
+
+
+    if 'python is fun' == guess:
+        print ('thanks this was made in python')   
+    elif guess == word:
+        win(tries)
+    elif guess == 'help':
+        print ('cmd for OS Terminal commands')
+        print ('Python for python logo')
+        time.sleep(1)
+        print ('if you want the word this bad...')
+        time.sleep(00.5)
+        print ('be ashamed of yourself because its...')
+        time.sleep(1)
+        print ('actually you should have a choice get out')
+        choice = input('Choice yes or no,will you get out:')
+        if choice == 'yes':
+            print ('good')
+        if choice == 'no':
+            time.sleep(1)
+            print ('its...')
+            print (word)
+            print ('now get out')
+            print (3)
+            time.sleep(1)
+            print (2)
+            time.sleep(1)
+            print(1)
+            time.sleep(1)
+            print('your going to the same fate as them...')
+            time.sleep(0.3)
+            print ('DIE')
+            time.sleep(1)
+            detonate(word)
+    else: 
+        print ('Wrong')
+        tries += 1
